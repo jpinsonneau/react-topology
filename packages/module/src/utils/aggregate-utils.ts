03f53de68500eab809bc479aab0cca6146f24e93
Julien Pinsonneau <jpinsonn@redhat.com>
2023-04-13 13:37:00 +0200

    code improved and cleanup

--- packages/module/src/utils/aggregate-utils.ts
+++ packages/module/src/utils/aggregate-utils.ts
@@ -1,17 +1,25 @@
 import { BaseEdge } from '../elements';
 import Point from '../geom/Point';
-import { Edge, GraphElement, ModelKind, Node } from '../types';
-import { getLinesIntersection } from './anchor-utils';
+import { Node, Edge, GraphElement, ModelKind } from '../types';
+import { distanceToPoint, getLinesIntersection } from './anchor-utils';
 
 interface Aggregate {
   pos: Point;
-  segments: [Point, Point][];
+  edge: BaseEdge;
+  segments: [Point, Point][] | null;
 }
 
-const getParentAtDepth = (element: GraphElement, depth: number) => {
+/**
+ * Get parent at specific depth
+ * @param element 
+ * @param depth 
+ * @param includeGraph 
+ * @returns 
+ */
+export const getParentAtDepth = (element: GraphElement, depth: number, includeGraph = true) => {
   let curr: GraphElement | null = element;
   while (curr && depth > 0) {
-    if (curr.hasParent()) {
+    if (curr.hasParent() && (includeGraph || curr.getParent().getKind() !== ModelKind.graph)) {
       curr = curr.getParent();
       depth--;
     } else {
@@ -21,102 +29,171 @@ const getParentAtDepth = (element: GraphElement, depth: number) => {
   return curr;
 };
 
-const getAggregatedPosition = (src: GraphElement, tgt: GraphElement, nestingDepth: number, edges: Edge[]) => {
-  if (src !== tgt) {
-    const segments = src.getKind() !== ModelKind.graph ? src.getLastSegments() : tgt.getLastSegments();
-    if (!segments) {
-      return null;
-    }
+/**
+ * Get element subnodes recursively
+ * @param element 
+ * @returns 
+ */
+export const getElementChildNodes = (element: GraphElement) => {
+  const nodes: Node[] = [];
 
-    let pos: Point | null = null;
-    edges.forEach(edge => {
-      let found: Node | null;
-      if (
-        getParentAtDepth(edge.getSource(), nestingDepth) === src &&
-        getParentAtDepth(edge.getTarget(), nestingDepth) === tgt
-      ) {
-        found = edge.getSource();
-      } else if (
-        getParentAtDepth(edge.getTarget(), nestingDepth) === src &&
-        getParentAtDepth(edge.getSource(), nestingDepth) === tgt
-      ) {
-        found = edge.getTarget();
+  element.getChildren().forEach(child => {
+    if (child.getKind() === ModelKind.node) {
+      const childNode = child as Node;
+      if (childNode.isGroup()) {
+        nodes.push(...getElementChildNodes(child));
+      } else {
+        nodes.push(childNode);
       }
+    }
+  });
 
-      if (found) {
-        const pointPos = found.getPosition().clone();
-        if (!found.isGroup()) {
-          const dimensions = edge.getSource().getDimensions();
-          pointPos.translate(dimensions.width / 2, dimensions.height / 2);
+  return nodes;
+}
+
+
+/**
+ * Returns edge aggregated positions and groups segments ordered by edge direction
+ * The groups are recursively traversed from deepest source group to destination until root is reached
+ * then from destination group -> source
+ * 
+ * @param edge 
+ * @returns 
+ */
+const getAggregatedPositions = (edge: BaseEdge) => {
+  const srcAggregates: Aggregate[] = [];
+  const tgtAggregates: Aggregate[] = [];
+
+  const edges = edge.getGraph().getEdges();
+
+  let topReached = false;
+  let nestingDepth = 1;
+  let curr: GraphElement | null = null;
+
+  function getEdgePeer(e: Edge | BaseEdge) {
+    return topReached ? e.getTarget() : e.getSource()
+  }
+
+  function setCurr() {
+    if (edge.getSource().getParent() !== edge.getTarget().getParent()) {
+      const peer = getEdgePeer(edge);
+      curr = getParentAtDepth(peer, nestingDepth, !topReached);
+    } else {
+      curr = null;
+    }
+  }
+
+  for (let i = 0; i < 2; i++) {
+    setCurr();
+    while (curr) {
+      const relatedPoints: Node[] = [];
+      edges.forEach(e => {
+        const parent = getParentAtDepth(getEdgePeer(e), nestingDepth);
+        if (parent === curr) {
+          const childNodes = getElementChildNodes(parent);
+          childNodes.forEach(c => {
+            if (!relatedPoints.includes(c)) {
+              relatedPoints.push(c);
+            }
+          });
         }
+      });
+
+      let pos: Point | null = null;
+      relatedPoints.forEach(p => {
+        const pointPos = p.getPosition().clone();
+        const dimensions = p.getDimensions();
+        pointPos.translate(dimensions.width / 2, dimensions.height / 2);
 
         if (pos) {
           pos.setLocation((pos.x + pointPos.x) / 2, (pos.y + pointPos.y) / 2);
         } else {
           pos = new Point(pointPos.x, pointPos.y);
         }
-      }
-    });
+      });
 
-    if (pos) {
-      return {
-        pos,
-        segments: segments.map(s => [new Point(s[0][0], s[0][1]), new Point(s[1][0], s[1][1])])
-      } as Aggregate;
-    }
-  }
-  return null;
-};
 
-const getAggregatedPositions = (src: GraphElement, tgt: GraphElement) => {
-  const aggregates: Aggregate[] = [];
-  const edges = src.getGraph().getEdges();
-
-  let curr = src;
-  const target = getParentAtDepth(tgt, 1);
-  if (target) {
-    let nestingDepth = 0;
-    while (curr) {
-      const srcAgg = getAggregatedPosition(curr, target, nestingDepth, edges);
-      if (srcAgg) {
-        aggregates.push(srcAgg);
-      }
-      const tgtAgg = getAggregatedPosition(target, curr, nestingDepth, edges);
-      if (tgtAgg) {
-        aggregates.push(tgtAgg);
-      }
-      nestingDepth++;
-      curr = getParentAtDepth(src, nestingDepth);
-    }
-  }
-
-  return aggregates;
-};
-
-const getAggregatedEdgeBendpoints = (edge: BaseEdge) => {
-  const allPoints: Point[] = [];
-  const aggregatedPositions = getAggregatedPositions(edge.getSource(), edge.getTarget());
-
-  if (aggregatedPositions.length > 1) {
-    for (let i = 0; i < aggregatedPositions.length; i++) {
-      const nextAgg = i >= aggregatedPositions.length / 2 ? i - 1 : i + 1;
-      const points = aggregatedPositions[i].segments;
-      for (let j = 0; j < points.length; j++) {
-        const nextPoint = j < points.length - 1 ? j + 1 : 0;
-        const intersectPoint = getLinesIntersection(
-          [aggregatedPositions[i].pos, aggregatedPositions[nextAgg].pos],
-          [points[j][0], points[nextPoint][0]]
-        );
-        if (intersectPoint) {
-          aggregatedPositions[i].pos = intersectPoint;
-          break;
+      if (pos) {
+        const agg = {
+          pos,
+          edge,
+          segments: curr.getLastSegments()?.map(s => [new Point(s[0][0], s[0][1]), new Point(s[1][0], s[1][1])])
+        } as Aggregate;
+        if (topReached) {
+          tgtAggregates.push(agg);
+        } else {
+          srcAggregates.push(agg);
         }
       }
-      allPoints.push(aggregatedPositions[i].pos);
+
+      nestingDepth++;
+      setCurr();
     }
+
+    // graph root has been reached, moving now from tgt to src
+    topReached = true;
+    nestingDepth = 1;
+  }
+
+  return [...srcAggregates, ...tgtAggregates.reverse()];
+};
+
+/**
+ * Returns the edge bendpoints aggregated per group
+ * 
+ * Example from A to E taking B,C,D positions in count:
+ * 
+ * Group 1 | root | Group 2
+ * ------------------------
+ *      A  |      |
+ *        \|      |
+ * B - - - x      |
+ *        /| \    |
+ *      C  |  \   |
+ *         |   \  |
+ *         |    \ |
+ *         |     \|
+ *         |      x - > E
+ *         |     /|
+ *         |    / |
+ *         |   D  |
+ * ------------------------
+ * @param edge 
+ * @returns 
+ */
+export const getAggregatedEdgeBendpoints = (edge: BaseEdge) => {
+  const allPoints: Point[] = [];
+  const aggregatedPositions = getAggregatedPositions(edge);
+
+  for (let i = 0; i < aggregatedPositions.length; i++) {
+    // check intersection between current aggregation and next one until target
+    const nextAgg = i < aggregatedPositions.length - 1 ?
+      aggregatedPositions[i + 1] : aggregatedPositions[i - 1];
+    const segments = aggregatedPositions[i].segments ?
+      aggregatedPositions[i].segments : nextAgg.segments;
+    if (segments) {
+      let bestDistance = Infinity;
+      let point: Point | null = null;
+      for (const points of segments) {
+        const intersectPoint = getLinesIntersection(
+          [aggregatedPositions[i].pos, nextAgg.pos],
+          points
+        );
+        if (intersectPoint) {
+          const intersectDistance = distanceToPoint(intersectPoint, aggregatedPositions[i].pos);
+          if (intersectDistance < bestDistance) {
+            point = intersectPoint;
+            bestDistance = intersectDistance;
+          }
+        }
+      }
+
+      if (point) {
+        allPoints.push(point);
+      }
+    }
+
   }
 
   return allPoints;
 };
-
-export { getAggregatedEdgeBendpoints };
